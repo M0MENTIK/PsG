60ed98aec6caeb20625044068fb35e1d6d07ebe6
M0MENTIK <ulanovvalerij42@gmail.com>
2026-02-03 09:38:35 +0300

    simple sql

--- main.go
+++ main.go
@@ -1,6 +1,46 @@
 package main
 
-func main() {
+import (
+	"PgS/feature_postgres/simple_connection"
+	"PgS/feature_postgres/simple_sql"
+	"context"
+	"fmt"
+	"time"
+)
 
-	CheckConnection()
+func main() {
+	ctx := context.Background()
+
+	conn, err := simple_connection.CreateConnection(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := simple_sql.CreateTable(ctx, conn); err != nil {
+		panic(err)
+	}
+
+	tasks, err := simple_sql.SelectRows(ctx, conn)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, task := range tasks {
+		if task.ID == 3 {
+			task.Title = "To feed cat"
+			task.Description = "To get cat 30 gram's of corm"
+			task.Completed = true
+			now := time.Now()
+			task.CompletedAt = &now
+
+			if err := simple_sql.UpdateTask(ctx, conn, task); err != nil {
+				panic(err)
+			}
+
+			break
+		}
+
+	}
+
+	fmt.Println("Succeed")
 }
